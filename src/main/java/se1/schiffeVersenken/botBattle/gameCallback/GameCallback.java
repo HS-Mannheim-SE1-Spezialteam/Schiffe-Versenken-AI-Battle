7d921532b8104e35166a46487c848a26d0520d76
Firestar99 <31222740+Firestar99@users.noreply.github.com>
2018-06-18 22:54:11 +0200

    BotBattle: forgot to push GameCallback changes

--- src/main/java/se1/schiffeVersenken/botBattle/gameCallback/GameCallback.java
+++ src/main/java/se1/schiffeVersenken/botBattle/gameCallback/GameCallback.java
@@ -32,4 +32,25 @@ public interface GameCallback {
 		};
 	}
 	
+	static GameCallback mergeCallback(GameCallback c1, GameCallback c2) {
+		return new GameCallback() {
+			@Override
+			public void init(Game game) {
+				c1.init(game);
+				c2.init(game);
+			}
+			
+			@Override
+			public void onShot(int id, boolean isSide1, Position position, Tile tile, Ship ship) {
+				c1.onShot(id, isSide1, position, tile, ship);
+				c2.onShot(id, isSide1, position, tile, ship);
+			}
+			
+			@Override
+			public void onGameOver(boolean isSide1) {
+				c1.onGameOver(isSide1);
+				c2.onGameOver(isSide1);
+			}
+		};
+	}
 }
