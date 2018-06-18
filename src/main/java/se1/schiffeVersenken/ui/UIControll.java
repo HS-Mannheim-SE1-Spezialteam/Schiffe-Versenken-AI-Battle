e62e1ea94a861be87b459f17e58a7d74df6116b3
Firestar99 <31222740+Firestar99@users.noreply.github.com>
2018-06-18 22:50:42 +0200

    GUI: UIControl ShipBorderConditions fix and cleanup

--- src/main/java/se1/schiffeVersenken/ui/UIControll.java
+++ src/main/java/se1/schiffeVersenken/ui/UIControll.java
@@ -1,21 +1,19 @@
 package se1.schiffeVersenken.ui;
 
-import javax.swing.JFrame;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
 import se1.schiffeVersenken.ReferenceAi.ReferencePlayerCreator;
 import se1.schiffeVersenken.botBattle.Game;
 import se1.schiffeVersenken.botBattle.PlayerInfo;
 import se1.schiffeVersenken.botBattle.gameCallback.ConsoleOutputCallback;
+import se1.schiffeVersenken.botBattle.gameCallback.GameCallback;
 import se1.schiffeVersenken.interfaces.GameSettings;
 import se1.schiffeVersenken.interfaces.GameSettingsBuilder;
-import se1.schiffeVersenken.interfaces.Ship;
 import se1.schiffeVersenken.ui.frames.game.GameArea;
 import se1.schiffeVersenken.ui.frames.setup.GameSetup;
 
-public class UIControll {
+import javax.swing.JFrame;
 
+public class UIControll {
+	
 	public static final JFrame setup = new GameSetup();
 //	public static final JFrame game = new GameArea();
 	
@@ -25,19 +23,13 @@ public class UIControll {
 	}
 	
 	public static void initGame(GameSettings.ShipBorderConditions border, int[] ships, boolean fastMode) {
-		GameSettingsBuilder builder = new GameSettingsBuilder().setShipBorderConditions(GameSettings.ShipBorderConditions.TOUCHING_ALLOWED);
-				
-		for(int i = 0; i < ships.length; i++) {
-			if(ships[i] > 0) builder.setNumOfShips(i + 1, ships[i]);
-		}
-				
+		GameSettingsBuilder builder = new GameSettingsBuilder()
+				.setShipBorderConditions(border);
+		for (int i = 0; i < ships.length; i++)
+			builder.setNumOfShips(i + 1, ships[i]);
+		
 		PlayerInfo playerInfo = new PlayerInfo(new ReferencePlayerCreator());
-		Thread trd = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				new Game(builder.createGameSettings(), playerInfo, playerInfo, new GameArea()).run();
-			}
-		});
+		Thread trd = new Thread(() -> new Game(builder.createGameSettings(), playerInfo, playerInfo, GameCallback.mergeCallback(new GameArea(), new ConsoleOutputCallback().setDelay(0))).run());
 		trd.start();
 	}
 }
