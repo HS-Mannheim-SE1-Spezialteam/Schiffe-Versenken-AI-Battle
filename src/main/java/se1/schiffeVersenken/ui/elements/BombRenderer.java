a345743203a11cc7f158e3d2a1fb2fba9f36254a
JTK222 <lukasporwit3@googlemail.com>
2018-06-25 19:14:54 +0200

    lighting up stuff

--- src/main/java/se1/schiffeVersenken/ui/elements/BombRenderer.java
+++ src/main/java/se1/schiffeVersenken/ui/elements/BombRenderer.java
@@ -1,9 +1,11 @@
 package se1.schiffeVersenken.ui.elements;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class BombRenderer extends ObjectRenderer{
 
+	private boolean lightUp = true;
 	private boolean ship = false;
 	public BombRenderer(int x, int y, boolean ship) {
 		super(x, y);
@@ -15,5 +17,10 @@ public class BombRenderer extends ObjectRenderer{
 		gfx.drawImage(objects,
 				(int) (sizeMultiplier * x), (int) (sizeMultiplier * y), (int) (sizeMultiplier * (x + 48)), (int) (sizeMultiplier * (y + 48)),
 				48, ship ? 48 : 0, 96, ship ? 96 : 48, null);
+		if(lightUp) {
+			gfx.setColor(new Color(255, 255, 255, 128));
+			gfx.fillRect((int) (sizeMultiplier * x), (int) (sizeMultiplier * y), 48, 48);
+			lightUp = false;
+		}
 	}
 }
